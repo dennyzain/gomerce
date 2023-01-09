c063794cd4929b59faeff6016c0fc7c2a75c46df
Denny Abbas Zain <abbasdenny24@gmail.com>
2023-01-09 22:09:35 +0700

    Slide list ListCategory
    - messy code

--- src/components/atoms/CardProduct.tsx
+++ src/components/atoms/CardProduct.tsx
@@ -8,8 +8,8 @@ interface IProps {
 
 const CardProduct: React.FC<IProps> = ({ title, imgProduct }) => {
   return (
-    <div className="border border-black-1 p-3">
-      <h4>{title}</h4>
+    <div className="border border-black-1 rounded-lg p-3 w-[120px]">
+      <h4 className="text-center">{title}</h4>
       <Image width={100} height={100} src={imgProduct} alt={title} />
     </div>
   );
--- src/components/molecules/ListCategory/index.tsx
+++ src/components/molecules/ListCategory/index.tsx
@@ -1,10 +1,20 @@
+import CardProduct from '@/components/atoms/CardProduct';
 import React from 'react';
 
 const ListCategory: React.FC = () => {
   return (
     <div className="p-6">
-      <h2>Pilihan Kategori</h2>
-      {/* {[1,2,3,4,5].map=>} */}
+      <h2 className="py-3">Pilihan Kategori</h2>
+      <div className="grid grid-flow-col auto-cols-max space-x-3 overflow-x-scroll">
+        {[1, 2, 3, 4, 5].map((item) => (
+          <CardProduct key={item} title="beras" imgProduct="/product1.png" />
+        ))}
+      </div>
+      <div className="pt-4 grid grid-flow-col auto-cols-max space-x-3 overflow-x-scroll">
+        {[1, 2, 3, 4, 5].map((item) => (
+          <CardProduct key={item} title="beras" imgProduct="/product1.png" />
+        ))}
+      </div>
     </div>
   );
 };
